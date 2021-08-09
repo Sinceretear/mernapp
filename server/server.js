2b22e4f98c9ad1c6c847dfa4c9774ea235b56d17
Hunter <bobnamed123@yahoo.com>
2021-08-09 10:28:41 -0700

    change

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
@@ -9,11 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-// get driver connection
 const dbo = require("./db/conn");
 
 app.get('/', (req, res) => {
-  res.send('just gonna send it');
+  res.send('just gonna send it .0005');
 });
 app.get('/flower', (req, res) => {
   res.json({
