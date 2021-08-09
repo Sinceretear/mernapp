e0fa30d04a3dc72e7df0dcc6ce21df61a2fc99fc
Hunter <bobnamed123@yahoo.com>
2021-08-09 11:43:14 -0700

    6

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8081;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
